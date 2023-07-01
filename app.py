e8f1b6e2a6edc5a6bec600332aafd649fa101a7b
hkahasha72 <110141564+hkahasha72@users.noreply.github.com>
2023-07-01 21:03:54 +1000

    added user and CLI command

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://hope:recycleandtrade123@localhost:5432'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://hope:recycleandtrade123@localhost:5432/eco'
 
 db = SQLAlchemy(app)
-print(db.__dict__)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('tables created successfully')
 
 @app.route('/')
 def index():
