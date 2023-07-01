d44029e848c2507c3d9fb268ffff192fe03e4084
hkahasha72 <110141564+hkahasha72@users.noreply.github.com>
2023-07-01 22:35:16 +1000

    added marshmallow and queries

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://hope:recycleandtrade123@localhost:5432/eco'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://hope:recycleandtrade123@localhost:5432/eco'
 
 db = SQLAlchemy(app)
 
@@ -15,8 +15,48 @@ class User(db.Model):
 
 @app.cli.command('create')
 def create_db():
-    db.create_all()
-    print('tables created successfully')
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        print('Tables created successfully')
+
+@app.cli.command('seed')
+def seed_db():
+    with app.app_context():
+      Users = [
+        User(
+            name='Hope',
+            email='hope@gmail.com',
+            password='hopek'
+        ),
+        User(
+            name='Mahi',
+            email='hope@gmail.com',
+            password='hopek'
+        ),
+        User(
+            name='kahasha',
+            email='hope@gmail.com',
+            password='hopek'
+        ),
+        ]
+
+    # Truncate the User table
+    db.session.query(User).delete()
+
+    # Add the User object to the session
+    db.session.add_all(Users)
+
+    # Commit the transaction to the database
+    db.session.commit()
+    print('Models seeded')
+
+@app.route('/users')
+def all_users():
+    # select * from "users";
+    stmt = db.select(User).order_by(User.name.email())
+    users = db.session.scalars(stmt).all()
+    return json.dumps(users)
 
 @app.route('/')
 def index():
@@ -24,3 +64,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
