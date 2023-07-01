9fc02fa736c6fd3a75544547c420b406637d0ef4
hkahasha72 <110141564+hkahasha72@users.noreply.github.com>
2023-07-01 20:46:18 +1000

    connected line

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, request
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://hope:recycleandtrade123@localhost:5432'
+
+db = SQLAlchemy(app)
+print(db.__dict__)
+
 @app.route('/')
 def index():
-    return 'Hello, world//'
+    return 'Hello, world'
 
 if __name__ == '__main__':
     app.run(debug=True)
